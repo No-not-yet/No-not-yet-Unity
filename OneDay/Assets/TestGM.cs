317b35f9c9a7294f7d5d8a2050420aaabb74d07e
CsrCornejo <cadalcito@gmail.com>
2018-03-06 11:58:59 -0600

    Added Player Status and GM

---
+++ OneDay/Assets/TestGM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Test of GM Manipulation
+		Debug.Log ("Hi, I'm: " + this.gameObject.name + " and I'm gonna change the GM");
+		Debug.Log("Game Manager has lvl as: " + GameManager.instance.getLevel ());
+		GameManager.instance.setLevel (3);
+		Debug.Log("Changed to: " + GameManager.instance.getLevel ());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ OneDay/Assets/TestStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStatus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Hi, I'm " + this.name + " and I'm gonna change the Player status");
+
+		Status playerStatus = GameObject.Find ("Player").GetComponent <Status>();
+		Debug.Log ("Player Busy status is: " + playerStatus.getBusy ());
+		playerStatus.setBusy (true);
+		Debug.Log ("Player Busy status changed to: " + playerStatus.getBusy ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
