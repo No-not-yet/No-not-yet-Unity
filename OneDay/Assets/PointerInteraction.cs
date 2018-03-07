58152808248c417bed939576d06c0e02ca6d7d50
CsrCornejo <cadalcito@gmail.com>
2018-03-06 21:32:13 -0600

    Cleaned Before Merge in dev

--- OneDay/Assets/PointerInteraction.cs
+++ OneDay/Assets/PointerInteraction.cs
@@ -9,47 +9,45 @@ public class PointerInteraction : MonoBehaviour
     private float timer;
     public float gazeTimer = 2f;
     private bool gazeAt;
-    public Material Click;
-    public Material NoClick;
 
     // Use this for initialization
     void Start()
     {
-        GetComponent<Renderer>().material = Click;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gazeAt)
-        {
-            timer += Time.deltaTime;
-            if (timer >= gazeTimer)
-            {
-                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-                timer = 0f;
-            }
-
-        }
+		//sendEventAfterSeconds ();
     }
+
+	public void sendEventAfterSeconds(){
+		if (gazeAt)
+		{
+			timer += Time.deltaTime;
+			if (timer >= gazeTimer)
+			{
+				ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+				timer = 0f;
+			}
+
+		}
+	}
 
     public void PointerEnter()
     {
-        
-
+		// Used for sendAfterSeconds
+		//gazeAt = true;
     }
 
     public void PointerExit()
     {
-        gazeAt = false;
-        GetComponent<Renderer>().material = NoClick;
+        //gazeAt = false;
     }
 
     public void PointerDown()
     {
-        GetComponent<Renderer>().material = Click;
         Debug.Log("Click");
-
-
     }
+		
 }
