28fb3c247a49fb785d952ddac8ff4efe522a6e9a
Hector Monter <monterhector@gmail.com>
2018-03-08 23:18:12 -0600

    added object list following player and cubeInteraction script

---
+++ OneDay/Assets/CubeInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CubeInteraction : MonoBehaviour
+{
+
+    public GameObject activList;
+
+
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activList.activeSelf == true && Input.GetMouseButtonDown(0))
+        {
+            activList.SetActive(false);
+        }
+
+
+    }
+
+    /*public void PointerEnter()
+    {
+        // Debug.Log("Pointer.enter");
+
+    }
+
+    public void PointerExit()
+    {
+         Debug.Log("Pointer.exit");
+
+    }*/
+
+    public void PointerDown()
+    {
+
+        activList.SetActive(true);
+        activList.GetComponent<Text>().text = "lol\nlol\nlol";
+
+    }
+
+}
